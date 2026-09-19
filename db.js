10b7f5dc496bb75b5c24262ca2d63d3cddec128a
gigi-josh <nehemiahjoshua885@gmail.com>
2026-09-19 02:23:53 +0100

    Create db.js

---
+++ db.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 10,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 5000,
+    ssl: { rejectUnauthorized: false }
+});
+
+module.exports = pool;
